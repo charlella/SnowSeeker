6642a69bbae1151e0a7a0f1c44e63294b373b984
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-04-23 23:12:49 +0200

    feat: Add NavigationSplitView

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationSplitView(preferredCompactColumn: .constant(.detail)) {
+            NavigationLink("Primary") {
+                Text("New view")
+            }
+        } detail: {
+            Text("Content")
+                .navigationTitle("Content View")
         }
-        .padding()
-    }
+        .navigationSplitViewStyle(.balanced)    }
 }
 
 #Preview {
