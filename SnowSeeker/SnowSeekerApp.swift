3c379b50d8ae9095ee541804b9d1f703b832ab68
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-04-23 22:47:56 +0200

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by charlene hoareau on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
